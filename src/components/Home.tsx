fc9410af4dc7eb6ab9714653b61190653288b659
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-11-20 19:07:53 +0530

    Added Home Page Initial Images

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,21 @@
+import main_1 from "../assets/main_1.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div
+        style={{ backgroundImage: `url(${main_1})` }}
+        className="h-[60vh] bg-cover bg-center"
+      >
+        <div>
+          <h1 className="text-white">Uniyal Tour and Travel</h1>
+          <p className="text-white">
+            Easy Online Taxi Booking Service in Uttarakhand
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
