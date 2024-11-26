1f9038a0acd5709f08ec846fa773324e0c438e04
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-11-26 21:04:18 +0530

    Modified Home File

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -115,69 +115,71 @@ const Home = () => {
         <h2 className="text-4xl font-bold text-gray-600">TOP DESTINATIONS</h2>
       </div>
 
-      <div className="grid grid-cols-3 text-center">
-        <div className="w-[350px]">
-          <img src={destination_1} />
-          <Destinations
-            heading="Dehradun to Jolly Grant Airport"
-            para="Enjoy seamless transfers to and from Jolly Grant Airport with our punctual Dehradun taxi service. Whether for a spiritual retreat in Rishikesh or departing after exploring Uttarakhand, our dedicated drivers ensure a comfortable journey."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_2} />
-          <Destinations
-            heading="Dehradun to New Delhi Airport"
-            para="Commence or conclude your journey seamlessly with our efficient pick and drop taxi service to and from Delhi Airport. With punctual drivers and well-maintained vehicles, we ensure timely arrivals, granting you peace of mind during travel."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_3} />
-          <Destinations
-            heading="Dehradun to Himachal Pradesh"
-            para="Embark on a journey to Himachal Pradesh's scenic wonders with our secure Dehradun taxi service. Explore Shimla, Manali, or trek the Himalayas with our experienced drivers for a memorable trip amidst breathtaking vistas."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_4} />
-          <Destinations
-            heading="Dehradun to Mussoorie"
-            para="Enjoy a scenic drive from Dehradun to the picturesque hill station of Mussoorie, nestled amidst the lush greenery of the Himalayas."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_5} />
-          <Destinations
-            heading="Dehradun to Rishikesh"
-            para="Dive into the spiritual ambiance of Rishikesh, known as the Yoga Capital of the World, with our hassle-free taxi service from Doon."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_6} />
-          <Destinations
-            heading="Dehradun to Haridwar"
-            para="Embark on a soulful journey to the sacred city of Haridwar, where the Ganges River flows in all its divine glory."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_7} />
-          <Destinations
-            heading="Dehradun to Delhi"
-            para="Travel between the vibrant city of Delhi and the serene landscapes of Dehradun with ease and convenience using our trusted taxi service."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_8} />
-          <Destinations
-            heading="Dehradun to Haryana"
-            para="xplore Haryana's essence with our dependable Dehradun taxi service. From historic Kurukshetra to urban Gurgaon, enjoy safe and stress-free travel to your destination."
-          />
-        </div>
-        <div className="w-[350px]">
-          <img src={destination_9} />
-          <Destinations
-            heading="Dehradun to Jaipur"
-            para="Our experienced drivers guarantee a comfortable journey, allowing you to explore majestic forts, bustling bazaars, and rich heritage sites stress-free."
-          />
+      <div className="flex justify-center">
+        <div className="flex flex-wrap text-center gap-2 max-w-[60%] border-4">
+          <div className="w-[350px] border-2">
+            <img src={destination_1} />
+            <Destinations
+              heading="Dehradun to Jolly Grant Airport"
+              para="Enjoy seamless transfers to and from Jolly Grant Airport with our punctual Dehradun taxi service. Whether for a spiritual retreat in Rishikesh or departing after exploring Uttarakhand, our dedicated drivers ensure a comfortable journey."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_2} />
+            <Destinations
+              heading="Dehradun to New Delhi Airport"
+              para="Commence or conclude your journey seamlessly with our efficient pick and drop taxi service to and from Delhi Airport. With punctual drivers and well-maintained vehicles, we ensure timely arrivals, granting you peace of mind during travel."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_3} />
+            <Destinations
+              heading="Dehradun to Himachal Pradesh"
+              para="Embark on a journey to Himachal Pradesh's scenic wonders with our secure Dehradun taxi service. Explore Shimla, Manali, or trek the Himalayas with our experienced drivers for a memorable trip amidst breathtaking vistas."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_4} />
+            <Destinations
+              heading="Dehradun to Mussoorie"
+              para="Enjoy a scenic drive from Dehradun to the picturesque hill station of Mussoorie, nestled amidst the lush greenery of the Himalayas."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_5} />
+            <Destinations
+              heading="Dehradun to Rishikesh"
+              para="Dive into the spiritual ambiance of Rishikesh, known as the Yoga Capital of the World, with our hassle-free taxi service from Doon."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_6} />
+            <Destinations
+              heading="Dehradun to Haridwar"
+              para="Embark on a soulful journey to the sacred city of Haridwar, where the Ganges River flows in all its divine glory."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_7} />
+            <Destinations
+              heading="Dehradun to Delhi"
+              para="Travel between the vibrant city of Delhi and the serene landscapes of Dehradun with ease and convenience using our trusted taxi service."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_8} />
+            <Destinations
+              heading="Dehradun to Haryana"
+              para="xplore Haryana's essence with our dependable Dehradun taxi service. From historic Kurukshetra to urban Gurgaon, enjoy safe and stress-free travel to your destination."
+            />
+          </div>
+          <div className="w-[350px]">
+            <img src={destination_9} />
+            <Destinations
+              heading="Dehradun to Jaipur"
+              para="Our experienced drivers guarantee a comfortable journey, allowing you to explore majestic forts, bustling bazaars, and rich heritage sites stress-free."
+            />
+          </div>
         </div>
       </div>
     </div>
